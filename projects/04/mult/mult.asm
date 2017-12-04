e20c329fce3f5ed2fc67d1050002a9319cfe3715
TsengWen <benson326789@gmail.com>
2017-12-04 22:31:36 +0800

    Add Mult.asm

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,46 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+// i = 0
+    @i
+    M=0
+// R2 =0
+    @2
+    M=0
+// if R0 == 0
+    @0
+    D=M
+    @END
+    D;JEQ
+// if R1 == 0
+    @1
+    D=M
+    @END
+    D;JEQ
+    
+(LOOP)
+//D = i
+    @i
+    D=M
+//D = i - R1
+    @1
+    D=D-M
+//if i - R1 < 0
+    @END
+    D;JEQ
+// R2 += R0
+    @0
+    D=M
+    @2
+    M=M+D
+// i++
+    @i
+    M=M+1
+//Goto LOOP
+    @LOOP
+    0;JMP   
+(END)
+    @END
+    0;JMP //Infinite loop
+
